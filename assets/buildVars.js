9ed99f4200c1edac741b3e87985d5ce30c9a9196
Christian Kester <christiankester77@gmail.com>
2023-05-21 17:24:25 -0400

    Fixed XML compiler

--- assets/buildVars.js
+++ assets/buildVars.js
@@ -1,34 +1,38 @@
 const converter = require("convert-csv-to-array");
 
 function buildVars(csvText) {
-    let xmlVars = `start`;
+    let xmlVars = ``;
     const csvData = converter.convertCSVToArray(csvText, { type: "array" });
     csvData.forEach((rowArr) => {
-        console.log(getVarParams(rowArr));
+        xmlVars = xmlVars.concat(getVarParams(rowArr));
     });
+
     return xmlVars;
 }
 
 function getVarParams(varRow) {
+    varRow = varRow.map((x) => x.replace(/(\r\n|\n|\r)/gm, ""));
     if (!varRow[0] | !varRow[1]) {
         throw new Error("Some variables are missing names or data types!");
     }
 
     let varString = `       <variables name="${varRow[0]}" typeName="${varRow[1]}"`;
     //variable is located in memory
-    //if (varRow[3]) {
-    varString.concat(` topologicalAddress="${varRow[3]}">`);
-    //} else {
-    //   varString.concat(`>`);
-    //}
+    if (varRow[3]) {
+        varString = varString.concat(` topologicalAddress="${varRow[3]}">
+`);
+    } else {
+        varString = varString.concat(`>
+`);
+    }
     //variable has a comment
     if (varRow[2]) {
-        varString.concat(
-            `
-        <comment>${varRow[2]}</comment>`
+        varString = varString.concat(
+            `           <comment>${varRow[2]}</comment>
+`
         );
     } else {
-        varString.concat(`</variables>
+        varString = varString.concat(`       </variables>
 `);
     }
 
